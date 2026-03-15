fa53a4663eff3260d6ceb435d5c1d80caa1abc59
Rainer Angerer <rainer.angerer@gmail.com>
2026-03-15 08:55:05 +0100

    add more features

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/blockchain-info-helpers" : "",
   turbopack: {
     root: import.meta.dirname,
   },
